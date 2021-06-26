a2aa9e33ba85935d39b755e4d7ef7754edd7c8fb
sarahondraszek <s1saondr@uni-trier.de>
2021-06-26 17:08:40 +0200

    some smol changes

--- preprocessing_and_training/create_arrays.py
+++ preprocessing_and_training/create_arrays.py
@@ -1,7 +1,7 @@
 from preprocessing_and_training.preprocess_with_yaml import save_data_to_array
 
 
-# Save data to array file first
+# Save data to array file first -> has to be done only once for a model!
 def create_arrays(feature_dim=40):
     """
     Feature dimension is set here but can be modified in the run-pipeline
@@ -10,4 +10,3 @@ def create_arrays(feature_dim=40):
     :return: None
     """
     save_data_to_array(max_len=feature_dim)
-
